675daed309d874c50edcb2944d0df35893d4e2e7
Fabrivj <fabrivj78@gmail.com>
2023-11-20 19:27:19 -0600

    Update SUMB10.asm

--- SUMB10.asm
+++ SUMB10.asm
@@ -4,31 +4,49 @@ ORG 1000H ; Set variables
 	CLOSE DB 01H
 	SNUM DB LNGTH DUP(030H)
 	CLOSE2 DB 01H
-	RESULT DB 9 DUP(030H)
+	RESULT DB LNGTH DUP(030H)
     CLOSE3 DB 00H
-	init_txt DB "Programa para sumar 2 numeros en base 10"
+	AMT_DI DB 03H
+	INIT_TXT DB "Programa para sumar 2 numeros en base 10"
 	NEND DB 00H
-	sec_txt DB "Ingrese el primer numero de maximo 8 digitos.  Ingrese enter para continuar" 
-	NEND2 DB 00H ;End of sec_txt
-	third_txt DB "Ingrese el segundo numero de maximo 8 digitos. Ingrese enter para continuar" 
-	NEND3 DB 00H ;End of third_txt
-	fo_txt DB "El resultado es:" 
-	NEND4 DB 00H ;End of third_txt
+	SEC_TXT DB "Ingrese el primer numero. Ingrese enter para continuar" 
+	NEND2 DB 00H ;End of SEC_TXT
+	THIRD_TXT DB "Ingrese el segundo numero. Ingrese enter para continuar" 
+	NEND3 DB 00H ;End of THIRD_TXT
+	FO_TXT DB "El resultado es:" 
+	NEND4 DB 00H ;End of THIRD_TXT
+	MDIGITS1 DB "De cuantas cifras es el numero?" 
+	NEND5 DB 00H ;End of THIRD_TXT
 ORG 3000H ;**********ALL Methods Here********
-PRINT_INIT:	MOV AL, OFFSET NEND - OFFSET init_txt ; Move to AL how many characters print
-	MOV BX, OFFSET init_txt ; Move to BX dir of init_txt
-	INT 7 ; INT 7 to print init_txt
+PRINT_INIT:	MOV AL, OFFSET NEND - OFFSET INIT_TXT ; Move to AL how many characters print
+	MOV BX, OFFSET INIT_TXT ; Move to BX dir of INIT_TXT
+	INT 7 ; INT 7 to print INIT_TXT
 	RET
-PRINT_SEC: MOV AL, OFFSET NEND2 - OFFSET sec_txt ; Move to AL how many characters print 	
-	MOV BX, OFFSET sec_txt ; Move to BX dir of sec_txt
-	INT 7 ; INT 7 to print sec_txt
+PRINT_DIG: MOV AL, OFFSET NEND5 - OFFSET MDIGITS1 ; Move to AL how many characters print 	
+	MOV BX, OFFSET MDIGITS1 ; Move to BX dir of MDIGITS1
+	INT 7 ; INT 7 to print SEC_TXT
+	RET
+GETDIG1: MOV BX, OFFSET AMT_DI
+	INT 6 ;INT 6 to get the int from user
+	SUB BYTE PTR[BX], 030H
+	MOV DL, [BX] ; Could skip
+	CMP DL, 08H
+	JNS EXIT
+	JS GETODIG1
+	EXIT: INT 0
+GETODIG1: RET
+PRINT_SEC: MOV AL, OFFSET NEND2 - OFFSET SEC_TXT ; Move to AL how many characters print 	
+	MOV BX, OFFSET SEC_TXT ; Move to BX dir of SEC_TXT
+	INT 7 ; INT 7 to print SEC_TXT
+	MOV AX, OFFSET CLOSE ; Prepare GETNUM1
+	SUB AL, AMT_DI
 	RET
 GETNUM1: MOV BX, AX
 	INT 6 ;INT 6 to get the int from user
-	MOV DL, [BX] 
+	MOV DL, [BX] ; Could skip
 	CMP BYTE PTR[BX], 0DH ;CMP if user hit enter
 	JZ GETOUT1 ; End Loop
-	DEC AX ;DEC 1 to AX dir
+	INC AX ;DEC 1 to AX dir
 	CALL GETNUM1 ;Call GETNUM1 to receive a new number
 GETOUT1: MOV BYTE PTR[BX], 030H 
 	RET ;END of GETNUM1
@@ -36,27 +54,36 @@ PRINTNUM1: MOV AL, OFFSET CLOSE - OFFSET FNUM ;Print the entire number entered b
 	MOV BX, OFFSET FNUM
 	INT 7
 	RET
-PRINT_THIRD: MOV AL, OFFSET NEND3 - OFFSET third_txt ; Move to AL how many characters print
-	MOV BX, OFFSET third_txt ; Move to BX dir of third_txt
-	INT 7 ; INT 7 to print third_txt
-	RET
+PRINT_THIRD: MOV AL, OFFSET NEND3 - OFFSET THIRD_TXT ; Move to AL how many characters print
+	MOV BX, OFFSET THIRD_TXT ; Move to BX dir of THIRD_TXT
+	INT 7 ; INT 7 to print THIRD_TXT
+	MOV AX, OFFSET CLOSE2 ; Prepare GETNUM2
+	SUB AL, AMT_DI
+	RET ; END OF PRINT_THIRD
 GETNUM_2: MOV BX, AX
 	INT 6 ;INT 6 to get the int from user
 	MOV DL, [BX] 
 	CMP BYTE PTR[BX], 0DH ;CMP if user hit enter
 	JZ GETOUT_2 ; End Loop
-	DEC AX ;DEC 1 to AX dir
+	INC AX ;DEC 1 to AX dir
 	CALL GETNUM_2 ;Call GETNUM2 to receive a new number
 GETOUT_2: MOV BYTE PTR[BX], 030H  
 	RET ;END of GETNUM2
 PRINT_N2: MOV AL, OFFSET CLOSE2 - OFFSET SNUM ;Print the entire number entered by user
 	MOV BX, OFFSET SNUM
 	INT 7
-	RET
-PRINT_FO: MOV AL, OFFSET NEND4 - OFFSET fo_txt ; Move to AL how many characters print 	
-	MOV BX, OFFSET fo_txt ; Move to BX dir of fo_txt
-	INT 7 ; INT 7 to print fo_txt
-	RET
+	RET ; END PRINT_N2
+PRINT_FO: MOV AL, OFFSET NEND4 - OFFSET FO_TXT ; Move to AL how many characters print 	
+	MOV BX, OFFSET FO_TXT ; Move to BX dir of FO_TXT
+	INT 7 ; INT 7 to print FO_TXT
+	RET ; PRINT_FO
+ORG_D:	MOV BX, OFFSET CLOSE
+	MOV CX, OFFSET CLOSE2
+	MOV DX, OFFSET CLOSE3
+	MOV CH, BL
+	MOV DH, DL
+	MOV AH, 00H
+	RET ; END ORG_D
 ADDI: DEC CH
 	DEC CL
 	DEC DH
@@ -79,27 +106,24 @@ ADDI: DEC CH
 	MOV [BX], AL 
 	CMP BX, OFFSET RESULT
 	JNZ ADDI
-	RET
+	RET ; END ADDI
 PRINT_RES: MOV AL, OFFSET CLOSE3 - OFFSET RESULT ;Print the entire number entered by user
 	MOV BX, OFFSET RESULT
 	INT 7
-	RET
+	RET ; END PRINT_RES
 ORG 2000H ; ***********Main program***********
 	CALL PRINT_INIT
+	CALL PRINT_DIG
+	CALL GETDIG1
 	CALL PRINT_SEC
-	MOV AX, OFFSET CLOSE - 1
 	CALL GETNUM1
 	CALL PRINTNUM1
+	CALL PRINT_DIG
+	CALL GETDIG1
 	CALL PRINT_THIRD
-	MOV AX, OFFSET CLOSE2 - 1
 	CALL GETNUM_2
 	CALL PRINT_N2
-	MOV BX, OFFSET CLOSE
-	MOV CX, OFFSET CLOSE2
-	MOV DX, OFFSET CLOSE3
-	MOV CH, BL
-	MOV DH, DL
-	MOV AH, 00H
+	CALL ORG_D
 	CALL ADDI
 	CALL PRINT_FO
 	CALL PRINT_RES
